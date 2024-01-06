b604a6edfc9e993f4808d416de5ca94db6f148fc
khan632 <rmustafa632@gmail.com>
2024-01-06 22:15:57 +0530

    update modification of feed and auth page

--- src/Layout/pageLaout/PageLayout.jsx
+++ src/Layout/pageLaout/PageLayout.jsx
@@ -1,23 +1,39 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Spinner } from "@chakra-ui/react";
 import { useLocation } from "react-router-dom";
-import SideBar from "../../components/SideBar/SideBar";
+import SideBar from "../../components/sideBar/SideBar";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../firebase/firebase";
+import Navbar from "../../components/navbar/Navbar";
 
-     // instead of adding sidebar component to every page, we can add it only once to the pageLayout 
-    // component and wrap the children with it. This way, we can have sidebar on every page except the Auth page
+// instead of adding sidebar component to every page, we can add it only once to the pageLayout
+// component and wrap the children with it. This way, we can have sidebar on every page except the Auth page
 
 const PageLayout = ({ children }) => {
   const { pathname } = useLocation();
+  const { user, loading } = useAuthState(auth);
+  const canSideBarRender = pathname !== "/auth" && user;
+  const canNavbarRender = !user && !loading && pathname !== "/auth";
   const sidebar = (
     <Box w={{ base: "70px", md: "240px" }}>
       <SideBar />
     </Box>
   );
+
+  const checkingUserIsAuth = !user && loading;
+  if (checkingUserIsAuth) return <PageLayoutSpinner />;
   return (
-    <Flex>
+    <Flex flexDir={canNavbarRender ? "column" : "row"}>
       {/* sidebar */}
-      {pathname !== "/auth" && sidebar}
+      {canSideBarRender && sidebar}
+
+      {/* Navbar */}
+      {canNavbarRender && <Navbar />}
       {/* main content (feed, friend suggestion box) */}
-      <Box flex={1} w={{ base: "calc(100% - 70px)", md: "calc(100% - 240px)" }}>
+      <Box
+        flex={1}
+        w={{ base: "calc(100% - 70px)", md: "calc(100% - 240px)" }}
+        mx={"auto"}
+      >
         {children}
       </Box>
     </Flex>
@@ -25,3 +41,16 @@ const PageLayout = ({ children }) => {
 };
 
 export default PageLayout;
+
+const PageLayoutSpinner = () => {
+  return (
+    <Flex
+      flexDir={"column"}
+      h="100vh"
+      alignItems="center"
+      justifyContent={"center"}
+    >
+      <Spinner size="xl" />
+    </Flex>
+  );
+};
--- src/hooks/useLogOut.js
+++ src/hooks/useLogOut.js
@@ -1,16 +1,18 @@
 import { useSignOut } from "react-firebase-hooks/auth";
 import useShowAlert from "./useShowAlert";
 import { auth } from "../firebase/firebase";
+import useAuthStore from "../store/authStore";
 
 function useLogOut() {
   const [ signOut, isLoggingOut, error ] = useSignOut(auth);
   const showAlert = useShowAlert();
+  const userLogout = useAuthStore((state) => state.logout);
 
   const handleLogout = async () => {
     try {
         await signOut();
         localStorage.removeItem("user-insta-info");
-        console.log("logout")
+        userLogout();
     } catch (err) {
       showAlert("Error", err.message, "error");
     }
